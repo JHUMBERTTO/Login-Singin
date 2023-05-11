6a8d427402c5b18d5ec70a28ffd7db22eed36715
Jhumberto <jhumbertto@gmail.com>
2023-05-11 16:22:54 -0600

    Config React-Router-Dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import { Routes, Route } from "react-router-dom"
+import Home  from './components/Home'
+import Login  from './components/Login'
+import Register  from './components/Register'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className="bg-gray-500 h-screen text-white flex">
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/register" element={<Register/>}/>
+      </Routes>
+    </div>
   )
 }
 
