6e99019e967c5e5ce04e28fdef2df302587ffc4d
Jhumberto <jhumbertto@gmail.com>
2023-05-11 19:49:29 -0600

    Config Context

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,18 @@ import { Routes, Route } from "react-router-dom"
 import Home  from './components/Home'
 import Login  from './components/Login'
 import Register  from './components/Register'
+import { AuthProvider } from "./context/authContext"
 
 const App = () => {
   return (
     <div className="bg-gray-500 h-screen text-white flex">
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/login" element={<Login/>}/>
-        <Route path="/register" element={<Register/>}/>
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/register" element={<Register/>}/>
+        </Routes>
+      </AuthProvider>
     </div>
   )
 }
---
+++ src/context/authContext.js
@@ -0,0 +1,16 @@
+import {Children, createContext} from 'react';
+
+const context =createContext()
+
+
+export function AuthProvider ({children}){
+  const user ={
+    login: true
+  }
+
+  return(
+    <context.Provider value={{user}}>
+      {children}
+    </context.Provider>
+  )
+}
