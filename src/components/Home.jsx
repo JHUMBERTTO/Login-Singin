cd1dbd080afc245aa2328b3774f6c8c50ff9300d
Jhumberto <jhumbertto@gmail.com>
2023-05-13 10:20:04 -0600

    Home styles

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -14,13 +14,15 @@ const Home = () => {
   
   
   return (
-    <div>
-      <h1>
-        Welcome{user.displayName || user.email}
-      </h1>
-      <button onClick={handleLogout}>
-        logout
-      </button>
+    <div className="w-full max-w-md m-auto text-white">
+      <div className="bg-gray-700 shadow-lg rounded-3xl px-8 pt-6 pb-8 mb-4">
+        <h1>
+          Welcome {user.displayName || user.email}
+        </h1>
+        <div className='flex justify-end mt-10'>
+          <button onClick={handleLogout} className='bg-sky-600 text-white  hover:bg-sky-700 font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline'>Logout</button>
+        </div>
+      </div>
     </div>
   )
 }
